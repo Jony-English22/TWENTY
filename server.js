1e5614f663eb65a3fb6390a5c1899b469a573c5d
Jony-English22 <jonathanrp200322@gmail.com>
2026-02-23 20:26:37 -0600

    Agregando Frontend

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const app = express();
-
 const PORT = process.env.PORT || 3000;
 
+// Indicar a Express dónde están los archivos estáticos
 app.use(express.static("public"));
 
+// Opcional: servir index.html para la raíz
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+// Ejemplo de API
 app.get("/api/test", (req, res) => {
-  res.json({ mensaje: "Backend funcionando en Azure 🚀" });
+  res.json({ mensaje: "Backend funcionando 🚀" });
 });
 
 app.listen(PORT, () => {
